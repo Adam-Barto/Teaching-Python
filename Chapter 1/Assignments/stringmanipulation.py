91f52d5211b2ba417a08f1453c2bda2cae60dffe
Adam-Barto <adam.jbarto@gmail.com>
2023-07-07 15:00:48 -0400

    Testing for Lowercase and Uppercase written, as well as the worksheet

---
+++ Chapter 1/Assignments/stringmanipulation.py
@@ -0,0 +1,25 @@
+
+
+def make_lowercase(the_string):
+    return None
+
+def make_uppercase(the_string):
+    return None
+
+def reverse(the_string):
+    return None
+
+def cut_string_in_half(the_string):
+    return None
+
+def remove_character(the_string, the_character):
+    return None
+
+def remove_word(the_string, the_word):
+    return None
+
+def remove_vowels(the_string):
+    return None
+
+def remove_list_of_characters(the_characters):
+    return None
---
+++ Chapter 1/Tests/test_stringmanipulation.py
@@ -0,0 +1,45 @@
+from unittest import TestCase
+import stringmanipulation as sm
+
+
+class Test(TestCase):
+    def test_make_lowercase(self):
+        tests = [
+            ["HELLO THERE!", "hello there!"],
+            ["Try this one On For Size.", "try this one on for size."],
+            ["We're Number 1!", "we're number 1!"]
+        ]
+        for the_string, expected in tests:
+            with self.subTest(f"{the_string} -> {expected}"):
+                actual = sm.make_lowercase(the_string)
+                self.assertEqual(actual, expected)
+
+    def test_make_uppercase(self):
+        tests = [
+            ["hello there!", "HELLO THERE!"],
+            ["Try this one On For Size", "TRY THIS ONE ON FOR SIZE"],
+            ["we're number 1!", "WE'RE NUMBER 1!"]
+        ]
+        for the_string, expected in tests:
+            with self.subTest(f"{the_string} -> {expected}"):
+                actual = sm.make_lowercase(the_string)
+                self.assertEqual(actual, expected)
+
+
+    def test_reverse(self):
+        self.fail()
+
+    def test_cut_string_in_half(self):
+        self.fail()
+
+    def test_remove_character(self):
+        self.fail()
+
+    def test_remove_word(self):
+        self.fail()
+
+    def test_remove_vowels(self):
+        self.fail()
+
+    def test_remove_list_of_characters(self):
+        self.fail()
